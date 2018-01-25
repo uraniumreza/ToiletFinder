55acba84409e958c469d85d9976db4e606fcf990
uraniumreza <reza.uranium@gmail.com>
2018-01-25 14:16:03 +0600

    update build tools

--- android/app/src/main/java/com/toiletfinder/MainApplication.java
+++ android/app/src/main/java/com/toiletfinder/MainApplication.java
@@ -3,6 +3,10 @@ package com.toiletfinder;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.react.rnspinkit.RNSpinkitPackage;
+import com.airbnb.android.react.maps.MapsPackage;
+import com.imagepicker.ImagePickerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -22,7 +26,11 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new VectorIconsPackage(),
+            new RNSpinkitPackage(),
+            new MapsPackage(),
+            new ImagePickerPackage()
       );
     }
 
